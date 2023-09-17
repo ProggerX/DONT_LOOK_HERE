2ed3c5be9f3138d80e7708285a77a1ed8d2ff96d
ProggerX <x@proggers.ru>
2023-09-17 07:20:53 +0000

    com

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def main():
     n = int(input())
     arr = map(int, input().split())
-    summ = 0
+    arr2 = []
     for i in arr:
-        summ += i*i
-    print("YES") if len(str(summ)) == 5 else print("NO")
+        if i >= 0: arr2.append(i)
+    print(*arr2)
 
 if __name__ == "__main__":
     main()
